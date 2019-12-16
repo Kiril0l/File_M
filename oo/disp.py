dea944e80412cfd73a63422bff4505f5c2a12265
Kiril0l <Kirill_1998_12@mail.ru>
2019-12-16 22:59:28 +0300

    f

--- oo/disp.py
+++ oo/disp.py
@@ -1,14 +1,25 @@
 import psutil
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
 for p in psutil.process_iter():
     try:
         path = p.exe()
     except psutil.AccessDenied:
         path = None
     p_dict = p.as_dict()
-    proc = {
-        "process_name": p_dict['name'],
-        "process_id": p.pid,
-        "path_file": path,
-        'memory': p_dict['memory_percent']
-    }
+    proc = [{
+        "proc_name": p_dict['name'],
+        "pid": p.pid,
+        "gg": path,
+        "HH": p_dict['memory_percent']
+    }]
     print(proc)
+
+proc_list = Gtk.ListStore(str, int, str, float)
+for item in proc:
+    proc_list.append(list(item))
+for row in proc_list:
+    print(row[:])
